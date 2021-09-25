b60981332c8cfa145e55d5a7ff911ff48e75c70a
DaniaMunoz <daniamildrevm.99@gmail.com>
2021-09-24 20:47:42 -0500

    Comentario en entidad persona

--- HospiEnCasa.App/HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -9,12 +9,12 @@ namespace HospiEnCasa.App.Dominio
     public class Persona 
     {
         //Identificador único de cada Persona
-        public int Id{get;set;}//No refactorizamos
-        [Required (ErrorMessage= "Ingrese el nombre")]
-        
+        public int Id{get;set;}//No refactorizamoss
+        [Required (ErrorMessage= "Ingrese el Nombre")]
+        //Nombre de cada persona
         public string Nombre{get;set;}
-        [Required (ErrorMessage= "Ingrese el apellido")]
-        
+        [Required (ErrorMessage= "Ingrese el Apellido")]
+        //Apellidos de cada persona 
         public string Apellidos{get;set;}
         [Required (ErrorMessage= "Ingrese el número de teléfono")]
         public string NumeroTelefono{get;set;}
